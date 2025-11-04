01d3ffa5a9cfdc84c419247f2c9f73fbc2c896be
lvallone3315 <55007696+lvallone3315@users.noreply.github.com>
2025-11-03 22:52:45 -0500

    class-669 - added testing getPlayer() returns the correct enum.

--- TicTacToeTest/AdditionalBoardTests.cpp
+++ TicTacToeTest/AdditionalBoardTests.cpp
@@ -29,5 +29,47 @@ namespace TicTacToeTest
 		TEST_METHOD(HelloAdditionalTests) {
 			Logger::WriteMessage("Running additional tests on the Board object");
 		}
+
+		// the following test expands on the test verifying player X starts
+		//   and verifies 
+		//      getPlayer() returns the correct enum &
+		//      nextPlayer() correctly switches from X to O &
+		//        also correctly switches from O back to X
+		TEST_METHOD(ExpandedGameStartPlayerX)
+		{
+			Logger::WriteMessage("Testing X is selected as first player\n");
+			// verify X is to play, O is NOT to play
+			Assert::AreEqual(board.getPlayerName(), 'X');
+			Assert::AreNotEqual(board.getPlayerName(), 'O');
+
+			// expanding test to verify the enum returned is X
+			Logger::WriteMessage("Testing enum returned by getPlayer() is also X\n");
+			Assert::AreEqual(static_cast<int>(TicTacToeBoard::X),
+				static_cast<int>(board.getPlayer()));
+
+			// test switching to player O
+			Logger::WriteMessage("Switch players via board.nextPlayer() to O, and verify all methods\n");
+			board.nextPlayer();
+			//    getPlayer() enum
+			Assert::AreEqual(static_cast<int>(TicTacToeBoard::O),
+				static_cast<int>(board.getPlayer()));
+			Assert::AreNotEqual(static_cast<int>(TicTacToeBoard::X),
+				static_cast<int>(board.getPlayer()));
+			//    getPlayerName() character
+			Assert::AreEqual(board.getPlayerName(), 'O');
+			Assert::AreNotEqual(board.getPlayerName(), 'X');
+
+			// test switching back to player X
+			Logger::WriteMessage("Switch players via board.nextPlayer() back to X, and verify all methods\n");
+			board.nextPlayer();
+			//    getPlayer() enum
+			Assert::AreEqual(static_cast<int>(TicTacToeBoard::X),
+				static_cast<int>(board.getPlayer()));
+			Assert::AreNotEqual(static_cast<int>(TicTacToeBoard::O),
+				static_cast<int>(board.getPlayer()));
+			//    getPlayerName() character
+			Assert::AreEqual(board.getPlayerName(), 'X');
+			Assert::AreNotEqual(board.getPlayerName(), 'O');
+		}
 	};
 }
